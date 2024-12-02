5dc80498565a2580bd8762c99236869bb8bf4028
tyrone-offshorly <188826837+tyrone-offshorly@users.noreply.github.com>
2024-12-02 16:31:54 +0800

    Add boilerplate code
    Implemented functions for library functionality

---
+++ script.js
@@ -0,0 +1,14 @@
+const myLibrary = [];
+
+function Book(title, author, pages, isRead) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.isRead = isRead;
+}
+
+function addBookToLibrary(title, author, pages, isRead) {
+  const newBook = new Book(title, author, pages, isRead);
+
+  myLibrary.push(newBook);
+}
