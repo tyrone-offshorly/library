d85b012806850ed9bdf9c55a2d6dc01e2f7c1acb
tyrone-offshorly <188826837+tyrone-offshorly@users.noreply.github.com>
2024-12-03 15:37:55 +0800

    Add buttons for deletion and toggle

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ function Book(title, author, pages, isRead) {
   this.author = author;
   this.pages = pages;
   this.isRead = isRead;
+
+  this.toggleIsRead = function () {
+    this.isRead = !this.isRead;
+  }
 }
 
 function addBookToLibrary(title, author, pages, isRead) {
@@ -13,28 +17,58 @@ function addBookToLibrary(title, author, pages, isRead) {
   myLibrary.push(newBook);
 }
 
+function deleteBook(index) {
+  myLibrary.splice(index, 1);
+  displayLibrary();
+}
+
+function toggleRead(index) {
+  myLibrary[index].toggleIsRead();
+  displayLibrary();
+}
+
+
+
 function displayLibrary() {
   const libraryContainer = document.querySelector('.library-container');
   libraryContainer.textContent='';
-  for(let book of myLibrary) {
+  myLibrary.forEach((book, index) => {
     const card = document.createElement('div');
     card.classList.add('card');
+    card.setAttribute("data-index", index);
+    
+    const detailWrapper = document.createElement('div');
+    detailWrapper.classList.add('detail-wrapper');
+
     const titleP = document.createElement('p');
     const authorP = document.createElement('p');
     const pagesP = document.createElement('p');
     const isReadP = document.createElement('p');
-
+    const toggleButton = document.createElement('button');
+    const deleteButton = document.createElement('button');
+    toggleButton.classList.add('toggle-button');
+    deleteButton.classList.add('delete-button');
+    
+    deleteButton.addEventListener('click',() => deleteBook(index));
+    toggleButton.addEventListener('click', () => toggleRead(index));
     titleP.textContent = book.title;
     authorP.textContent = book.author;
-    pagesP.textContent = book.pages;
+    pagesP.textContent = book.pages + " pages";
     isReadP.textContent = book.isRead ? 'Read' : 'Not yet read';
-    card.appendChild(titleP);
-    card.appendChild(authorP);
-    card.appendChild(pagesP);
-    card.appendChild(isReadP);
+    toggleButton.textContent = "Toggle read";
+    deleteButton.textContent = "Delete"
+
+    detailWrapper.appendChild(titleP);
+    detailWrapper.appendChild(authorP);
+    detailWrapper.appendChild(pagesP);
+    detailWrapper.appendChild(isReadP);
+
+    card.append(detailWrapper);
+    card.appendChild(toggleButton);
+    card.appendChild(deleteButton);
 
     libraryContainer.appendChild(card);
-  }
+  })
 }
 
 // For demonstration purposes only
